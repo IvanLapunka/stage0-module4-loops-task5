20452195225f815dded1e0b1d0a6b7a6def9e020
Ivan Lapunka <ivan_lapunka@gmail.com>
2023-03-02 19:45:37 +0300

    Changes

--- src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
+++ src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
@@ -2,6 +2,16 @@ package school.mjc.stage0.loops.task5;
 
 public class Hourglass {
     public void printHourglassOfGivenSize(int height) {
-
+        for (int i = 0; i <= height; i++){
+            if (i == height / 2) continue;
+            for (int j = 0; j < height; j++) {
+                if (j >= Math.min(i, height - i) && j < height - Math.min(i, height -i)){
+                    System.out.print(8);
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
     }
 }
